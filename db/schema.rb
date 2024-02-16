7cc59375939161786a16a58879295fc4e2e5dd25
phillip-nguyen582913 <phillipnguyen456@gmail.com>
2024-02-16 12:20:54 -0600

    login screen

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_02_12_180626) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "clients", force: :cascade do |t|
     t.string "clientName"
     t.integer "ordersPlaced"
