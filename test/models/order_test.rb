d18facd7cb110e1e86bfbe84fd4966112fc9f8eb
ann-nguyen614 <ann.nguyen614@gmail.com>
2024-03-21 20:21:31 -0500

    test cases

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,7 +1,38 @@
 require "test_helper"
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Ensure Gallons included" do
+    order = Order.new
+    assert_not order.save, "Saved Order without Gallons Requested filled in"
+  end
+
+  test "Ensure Profit Margin Included" do
+    order = Order.new
+    assert_not order.save, "Saved Order without Profit Margins filled in"
+  end
+
+  test "Ensure Delivery Address included" do
+    order = Order.new
+    assert_not order.save, "Saved Order without Delivery Address filled in"
+  end
+
+  test "Ensure City included" do
+    order = Order.new
+    assert_not order.save, "Saved Order without City filled in"
+  end
+
+  test "Ensure State included" do
+    order = Order.new
+    assert_not order.save, "Saved Order without State filled in"
+  end
+
+  test "Ensure Zip Code included" do
+    order = Order.new
+    assert_not order.save, "Saved Order without Zipcode filled in"
+  end
+
+  test "Ensure Delivery Date included" do
+    order = Order.new
+    assert_not order.save, "Saved Order without any requested Delivery Date"
+  end
 end
--- test/models/profile_test.rb
+++ test/models/profile_test.rb
@@ -1,7 +1,45 @@
 require "test_helper"
 
 class ProfileTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Ensure Full Name included" do
+    profile = Profile.new
+    assert_not profile.save, "Saved Profile without Full Name"
+  end
+
+  test "Ensure Address One included" do
+    profile = Profile.new
+    assert_not profile.save, "Saved Profile without Address One"
+  end
+
+  test "Ensure City included" do
+    profile = Profile.new
+    assert_not profile.save, "Saved Profile without City"
+  end
+
+  test "Ensure State included" do
+    profile = Profile.new
+    assert_not profile.save, "Saved Profile without State"
+  end
+
+  test "Ensure Zipcode included" do
+    profile = Profile.new
+    assert_not profile.save, "Saved Profile without Zipcode"
+  end
+
+  test "Ensure User ID included" do
+    profile = Profile.new
+    assert_not profile.save, "Saved Profile without User ID"
+  end
+
+  test "Ensure Profile saves with all required fields" do
+    profile = Profile.new(
+      fullName: "",
+      addressOne: "",
+      city: "",
+      state: "",
+      zipcode: "",
+      user_id: users(:one).id
+    )
+    assert profile.save, "Could not save Profile with all required fields"
+  end
 end
