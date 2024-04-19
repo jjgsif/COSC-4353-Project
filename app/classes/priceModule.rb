9c5a821a35d257af0ae83e6448fe936fead693a2
Phillip Nguyen <phillipnguyen456@gmail.com>
2024-04-19 13:04:07 -0500

    incomplete pricing model

--- app/classes/priceModule.rb
+++ app/classes/priceModule.rb
@@ -1,4 +1,26 @@
-class priceModule
-    def initialize()
-    end
+class PriceModule
+  def initialize(location, gallons_requested, user_id)
+    @current_price_per_gallon = 1.50
+    @location_factor = location == "TX" ? 0.02 : 0.04
+    @rate_history_factor = has_rate_history?(user_id) ? 0.01 : 0.00
+    @gallons_requested_factor = gallons_requested > 1000 ? 0.02 : 0.03
+    @company_profit_factor = 0.10
+  end
+
+  def calculate_price_per_gallon
+    margin = (@location_factor - @rate_history_factor + @gallons_requested_factor + @company_profit_factor) * @current_price_per_gallon
+    suggested_price_per_gallon = @current_price_per_gallon + margin
+    suggested_price_per_gallon.round(3) # Round to 3 decimal places
+  end
+
+  def calculate_total_amount_due(gallons_requested)
+    total_amount_due = gallons_requested * suggested_price_per_gallon
+    total_amount_due # No need to round here, already rounded in calculate_price_per_gallon
+  end
+
+  private
+
+  def has_rate_history?(user_id)
+    Order.exists?(user_id: user_id)
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -7,5 +7,15 @@ class Order < ApplicationRecord
     validates :city, presence:true
     validates :state, presence: true, length: {is: 2}, inclusion: ["AL", "AK", "AZ", "AR", "TX", "CO", "CT", "CA","DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC","ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "UT", "VT", "VA", "WA", "WV", "WI","WY"]
     validates :zip, length: { is: 5 }, presence: true
+    # Method to calculate price per gallon
+    def calculate_price_per_gallon
+        price_module = PriceModule.new(state, gallonsRequested, user_id)
+        price_module.calculate_price_per_gallon
+    end
 
+    # Method to calculate total amount due
+    def calculate_total_amount_due
+        price_module = PriceModule.new(state, gallonsRequested, user_id)
+        price_module.calculate_total_amount_due(gallonsRequested)
+    end
 end
