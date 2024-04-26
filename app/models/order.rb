063bb0532ffc2c069555829193af9f5348c79f15
Phillip Nguyen <phillipnguyen456@gmail.com>
2024-04-26 11:27:21 -0500

    Revert "Changed ordering and profile views"
    This reverts commit fb43072887d859f81971bdc7e21a8e388611056d.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,12 +1,6 @@
 class Order < ApplicationRecord
     belongs_to :user
-    
-    @state = ["AL", "AK", "AZ", "AR", "TX", "CO", "CT", "CA","DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC","ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "UT", "VT", "VA", "WA", "WV", "WI","WY"]
-
-    def self.state
-        @state
-    end
-
+  
     validates :gallonsRequested, presence:true
     validates :deliveryDate, presence:true
     validates :deliveryAddress, presence:true
@@ -23,14 +17,5 @@ class Order < ApplicationRecord
     def calculate_total_amount_due
         price_module = PriceModule.new(state, gallonsRequested, user_id, id)
         price_module.calculate_total_amount_due(gallonsRequested)
-    end
-
-    validate :date_is_greater_than_today, on: :create # you want on create, otherwise a record becomes invalid after time
-
-    private
-
-    def date_is_greater_than_today
-        return unless self.deliveryDate.present?
-        errors.add(:deliveryDate, 'must be greater than today') unless self.deliveryDate > Date.today
     end
 end
